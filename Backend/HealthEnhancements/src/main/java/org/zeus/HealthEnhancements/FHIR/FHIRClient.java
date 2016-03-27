c90d6c12b8fcb1fc997057533397904aa3b96147
Ravi Khanuja <rkhanuja3@gatech.edu>
2016-03-27 01:42:42 -0400

    Added methods to retrieve patient history

--- Backend/HealthEnhancements/src/main/java/org/zeus/HealthEnhancements/FHIR/FHIRClient.java
+++ Backend/HealthEnhancements/src/main/java/org/zeus/HealthEnhancements/FHIR/FHIRClient.java
@@ -8,8 +8,14 @@ import org.hl7.fhir.instance.model.api.IBaseResource;
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.model.api.IResource;
 import ca.uhn.fhir.model.base.resource.BaseOperationOutcome;
+import ca.uhn.fhir.model.dstu2.composite.CodingDt;
 import ca.uhn.fhir.model.dstu2.resource.Bundle;
+import ca.uhn.fhir.model.dstu2.resource.Bundle.Entry;
+import ca.uhn.fhir.model.dstu2.resource.Claim.Diagnosis;
 import ca.uhn.fhir.model.dstu2.resource.Conformance;
+import ca.uhn.fhir.model.dstu2.resource.DiagnosticReport;
+import ca.uhn.fhir.model.dstu2.resource.Medication;
+import ca.uhn.fhir.model.dstu2.resource.MedicationStatement;
 import ca.uhn.fhir.model.dstu2.resource.Observation;
 import ca.uhn.fhir.model.dstu2.resource.OperationOutcome;
 import ca.uhn.fhir.model.dstu2.resource.OperationOutcome.Issue;
@@ -41,9 +47,9 @@ public class FHIRClient {
       return client;
     }
 
-    public Bundle searchPatientByFamilyName(String familyName) {
+    public Bundle searchPatientByFamilyName(String baseurl, String familyName) {
       
-      IGenericClient client = this.getClient("");
+      IGenericClient client = this.getClient(baseurl);
 
       // Perform a search
       Bundle results = client
@@ -55,9 +61,9 @@ public class FHIRClient {
       return results;
     }
 
-    public void createPatient(String first, String last, String pid){
+    public void createPatient(String baseurl, String first, String last, String pid){
 
-        IGenericClient client = this.getClient("");
+        IGenericClient client = this.getClient(baseurl);
       // START SNIPPET: create
          Patient patient = new Patient();
          // ..populate the patient object..
@@ -81,5 +87,81 @@ public class FHIRClient {
          IdDt id = (IdDt) outcome.getId();
          System.out.println("Got ID: " + id.getValue());
          // END SNIPPET: create
+    }
+    
+    public List getMedicationsByPatientId(String baseurl, String patientid, String startdate, String enddate){
+    	IGenericClient client = this.getClient(baseurl);
+    	Bundle ms = client.search()
+				.forResource(MedicationStatement.class).where(MedicationStatement.PATIENT.hasId(patientid))
+				.returnBundle(ca.uhn.fhir.model.dstu2.resource.Bundle.class)
+				.encodedJson().lastUpdated(new DateRangeParam(startdate, enddate)) // Date is of format "2014-11-01"
+				.execute();
+    	List<String> medications = new ArrayList<String>();
+		for (Entry entry : ms.getEntry()){
+			for(IResource iResource : entry.getResource().getContained().getContainedResources()){
+				if(iResource.getResourceName().toString().equals("Medication")){
+					Medication med = (Medication) iResource;
+					for(CodingDt code : med.getCode().getCoding()){
+						System.out.println("\n Medication Name :"+code.getDisplay());
+						medications.add(code.getDisplay());
+					}
+				}
+			}
+		}
+		return medications;
+
+    }
+    
+    public Bundle getObservationsByPatientId(String baseurl, String patientid, String startdate, String enddate){
+    	IGenericClient client = this.getClient(baseurl);
+    	Bundle bundle = client.search().
+                forResource(Observation.class).
+                where(Observation.PATIENT.hasId(patientid))
+                .encodedJson().lastUpdated(new DateRangeParam(startdate, enddate)) // Date is of format "2014-11-01"
+				.include(Observation.INCLUDE_PATIENT).returnBundle(Bundle.class).execute();
+
+        // Then
+        for (Bundle.Entry entry : bundle.getEntry()) {
+            if (entry.getResource() instanceof Observation) {
+                Observation observation = (Observation) entry.getResource();
+                CodingDt firstCoding = observation.getCode().getCodingFirstRep();
+                System.out.println("Observation: code = " + firstCoding.getSystem() + "|" + firstCoding.getCode() + "|" + firstCoding.getDisplay());
+            } else if (entry.getResource() instanceof  Patient) {
+                Patient patient = (Patient) entry.getResource();
+                System.out.println("Patient: name = " + patient.getNameFirstRep().getFamilyFirstRep() + ", " + patient.getNameFirstRep().getGivenFirstRep());
+            }
+        }
+        return bundle;
+    }
+    
+    public Bundle getDiagnosticReportByPatientId(String baseurl, String patientid, String startdate, String enddate){
+    	IGenericClient client = this.getClient(baseurl);
+    	Bundle bundle = client.search().
+                forResource(DiagnosticReport.class).
+                where(DiagnosticReport.PATIENT.hasId(patientid))
+                .encodedJson().lastUpdated(new DateRangeParam(startdate, enddate)) // Date is of format "2014-11-01"
+				.include(Observation.INCLUDE_PATIENT).returnBundle(Bundle.class).execute();
+
+        // Then
+        for (Bundle.Entry entry : bundle.getEntry()) {
+            if (entry.getResource() instanceof DiagnosticReport) {
+            	DiagnosticReport dreport = (DiagnosticReport) entry.getResource();
+                CodingDt firstCoding = dreport.getCode().getCodingFirstRep();
+                System.out.println("Observation: code = " + firstCoding.getSystem() + "|" + firstCoding.getCode() + "|" + firstCoding.getDisplay());
+            } else if (entry.getResource() instanceof  Patient) {
+                Patient patient = (Patient) entry.getResource();
+                System.out.println("Patient: name = " + patient.getNameFirstRep().getFamilyFirstRep() + ", " + patient.getNameFirstRep().getGivenFirstRep());
+            }
+        }
+        return bundle;
+    }
+    
+    public Bundle searchByPlainURL(IGenericClient client, String url){
+    	// Search URL can also be a relative URL in which case the client's base
+    	// URL will be added to it for example searchUrl = "Patient?identifier=foo"
+    	return client.search()
+    	      .byUrl(url)
+    	      .returnBundle(ca.uhn.fhir.model.dstu2.resource.Bundle.class)
+    	      .execute();
     }
 }
