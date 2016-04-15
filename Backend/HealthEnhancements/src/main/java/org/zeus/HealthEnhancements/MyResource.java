0f5afd30f73685f5a9a0ce4b6452dc430e958a35
Ravi Khanuja <rkhanuja3@gatech.edu>
2016-04-14 23:50:18 -0400

    Added the Access-Control-Allow-Origin header in the response

--- Backend/HealthEnhancements/src/main/java/org/zeus/HealthEnhancements/MyResource.java
+++ Backend/HealthEnhancements/src/main/java/org/zeus/HealthEnhancements/MyResource.java
@@ -10,15 +10,14 @@ import com.google.gson.GsonBuilder;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-
 
 import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
@@ -49,339 +48,435 @@ public class MyResource {
 	@POST
 	@Path ("CreateUser")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Object CreateUser(@HeaderParam("UserName") String szUserName, @HeaderParam("Password") String szPassword)
+	public void CreateUser(@HeaderParam("UserName") String szUserName, @HeaderParam("Password") String szPassword)
 	{
+		String returnObj = "";
 		UserInfo user = new UserInfo();
-
 		if(!user.CreateUser(szUserName, szPassword))
-			return "{\"error\":\"User already exists in the system. Please try again with a different username and password\"}";
+			returnObj =  "{\"error\":\"User already exists in the system. Please try again with a different username and password\"}";
 		else
-			return ObjectToString(user);	
+			returnObj =  ObjectToString(user);
+		
+		Response response = Response.status(200).
+                entity(returnObj).
+                header("Access-Control-Allow-Origin", "*").build();
 	}
 
 	@GET
 	@Path("UserLogin")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Object CheckUserIdAndPassword(@HeaderParam("UserName") String szUserName, @HeaderParam("Password") String szPassword)
+	public void CheckUserIdAndPassword(@HeaderParam("UserName") String szUserName, @HeaderParam("Password") String szPassword)
 	{	
+		String returnObj = "";
 		UserInfo user = new UserInfo();
 
 		if(!user.IsValidUser(szUserName, szPassword))
-			return "{\"error\":\"User not authenticated\"}";
+			returnObj =  "{\"error\":\"User not authenticated\"}";
 		else
-			return "{\"Success\":\"User authenticated\"}";
+			returnObj =  "{\"Success\":\"User authenticated\"}";
+		
+		Response response = Response.status(200).
+                entity(returnObj).
+                header("Access-Control-Allow-Origin", "*").build();
 	}
 
 	@GET
 	@Path("GetPatientProfile")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Object GetPatientProfile(@HeaderParam("UserName") String szUserName, @HeaderParam("Password") String szPassword)
+	public void GetPatientProfile(@HeaderParam("UserName") String szUserName, @HeaderParam("Password") String szPassword)
 	{
+		String returnObj = "";
+		
 		UserInfo user = new UserInfo();
 
 		try
 		{
 			user.LoadPatientProfile(szUserName, szPassword);
+			returnObj =  ObjectToString(user);
 		}
 		catch(Exception e)
 		{
-			return e.getMessage();
+			returnObj =  e.getMessage();
 
 		}
-		return ObjectToString(user);
+		
+		Response response = Response.status(200).
+                entity(returnObj).
+                header("Access-Control-Allow-Origin", "*").build();
 	}
 
 	@PUT
 	@Path("UpdatePatientProfile")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Object UpdatePatientProfile(@HeaderParam("userInfo") String szUser)
+	public void UpdatePatientProfile(@HeaderParam("userInfo") String szUser)
 	{
+		String returnObj = "";
+		
 		UserInfo user = StringToObject_UserInfo(szUser);
 
 		try
 		{
 			if(user.UpdatePatientProfile())
-				return "{\"success\":\"Patient profile updated\"}";	
+				returnObj =   "{\"success\":\"Patient profile updated\"}";	
 		}
 		catch(Exception e)
 		{
-			return e.getMessage();
+			
+			returnObj =   "{\"error\":\"Patient profile not updated: " +  e.getMessage() + "\"}";
 		}
-
-		return "{\"error\":\"Patient profile not updated\"}";
+		Response response = Response.status(200).
+                entity(returnObj).
+                header("Access-Control-Allow-Origin", "*").build();
+		
 	}
 
 	@POST
 	@Path ("CreateProvider")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Object CreateProvider(@HeaderParam("UserName") String szUserName, @HeaderParam("Password") String szPassword)
+	public void CreateProvider(@HeaderParam("UserName") String szUserName, @HeaderParam("Password") String szPassword)
 	{
+		String returnObj = "";
 		ProviderInfo provider = new ProviderInfo();
 
 		if(!provider.CreateProvider(szUserName, szPassword))
-			return "{\"error\":\"Provider already exists in the system. Please try again with a different username and password\"}";
+			returnObj =   "{\"error\":\"Provider already exists in the system. Please try again with a different username and password\"}";
 		else
-			return ObjectToString(provider);
+			returnObj =   ObjectToString(provider);
+		
+		Response response = Response.status(200).
+                entity(returnObj).
+                header("Access-Control-Allow-Origin", "*").build();
 	}
 
 	@GET
 	@Path("ProviderLogin")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Object CheckProviderIdAndPassword(@HeaderParam("UserName") String szUserName, @HeaderParam("Password") String szPassword)
+	public void CheckProviderIdAndPassword(@HeaderParam("UserName") String szUserName, @HeaderParam("Password") String szPassword)
 	{	
+		String returnObj = "";
 		ProviderInfo provider = new ProviderInfo();
 
 		if(!provider.IsValidUser(szUserName, szPassword))
-			return "{\"error\":\"Provider not authenticated\"}";
+			returnObj =   "{\"error\":\"Provider not authenticated\"}";
 		else
-			return "{\"Success\":\"Provider authenticated\"}";
+			returnObj =   "{\"Success\":\"Provider authenticated\"}";
+		
+		Response response = Response.status(200).
+                entity(returnObj).
+                header("Access-Control-Allow-Origin", "*").build();
 	}
 
 	@GET
 	@Path("GetProviderProfile")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Object GetProviderProfile(@HeaderParam("UserName") String szUserName, @HeaderParam("Password") String szPassword)
+	public void GetProviderProfile(@HeaderParam("UserName") String szUserName, @HeaderParam("Password") String szPassword)
 	{
+		String returnObj = "";
 		ProviderInfo provider = new ProviderInfo();
 
 		provider.LoadProviderProfile(szUserName, szPassword);
-		return ObjectToString(provider);
+		returnObj = ObjectToString(provider);
+		Response response = Response.status(200).
+                entity(returnObj).
+                header("Access-Control-Allow-Origin", "*").build();
 	}
 
 	@PUT
 	@Path("UpdateProviderProfile")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Object UpdateProviderProfile(@HeaderParam("providerInfo") String szProvider)
+	public void UpdateProviderProfile(@HeaderParam("providerInfo") String szProvider)
 	{
+		String returnObj = "";
 		ProviderInfo provider = StringToObject_ProviderInfo(szProvider);
 
 		if(!provider.UpdateProviderProfile())
-			return "{\"error\":\"Provider profile not updated\"}";
+			returnObj =  "{\"error\":\"Provider profile not updated\"}";
 		else
-			return "{\"success\":\"Provider profile updated\"}";	
+			returnObj =  "{\"success\":\"Provider profile updated\"}";	
+		
+		Response response = Response.status(200).
+                entity(returnObj).
+                header("Access-Control-Allow-Origin", "*").build();
 	}
 
 	@POST
 	@Path ("AddPatientToProviderMapping")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Object AddPatientToProviderMapping(@HeaderParam("PatientID") String szPatientID, 
+	public void AddPatientToProviderMapping(@HeaderParam("PatientID") String szPatientID, 
 			@HeaderParam("ProviderID") String szProviderID)
 	{
+		String returnObj = "";
 		PatientToProvider p2p = new PatientToProvider();
 
 		try
 		{
 			if(!p2p.AddPatientToProviderMapping(szPatientID, szProviderID))
-				return "{\"error\":\"Patient-Provider mapping not added to the system\"}";
+				returnObj =   "{\"error\":\"Patient-Provider mapping not added to the system\"}";
 			else
-				return "{\"success\":\"Patient-Provider mapping added to the system\"}";	
+				returnObj =   "{\"success\":\"Patient-Provider mapping added to the system\"}";	
 		}
 		catch(Exception e)
 		{
-			return e.getMessage();
+			returnObj =   e.getMessage();
 		}
+		Response response = Response.status(200).
+                entity(returnObj).
+                header("Access-Control-Allow-Origin", "*").build();
+		
 	}
 
 	@GET
 	@Path("IsValidPatientToProviderMapping")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Object IsValidPatientToProviderMapping(@HeaderParam("PatientID") String szPatientID, 
+	public void IsValidPatientToProviderMapping(@HeaderParam("PatientID") String szPatientID, 
 			@HeaderParam("ProviderID") String szProviderID)
 	{
+		String returnObj = "";
 		PatientToProvider p2p = new PatientToProvider();
 
 		if(!p2p.IsProviderPrivilegedToViewPatientData(szProviderID, szPatientID))
-			return "{\"error\":\"Provider cannot view patient data\"}";
+			returnObj = "{\"error\":\"Provider cannot view patient data\"}";
 		else
-			return "{\"success\":\"Provider can view patient data\"}";		
+			returnObj = "{\"success\":\"Provider can view patient data\"}";
+		
+		Response response = Response.status(200).
+                entity(returnObj).
+                header("Access-Control-Allow-Origin", "*").build();
 	}
 
 	@GET
 	@Path("GetAllProvidersForPatient")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Object GetAllProvidersForPatient(@HeaderParam("PatientID") String szPatientID)
+	public void GetAllProvidersForPatient(@HeaderParam("PatientID") String szPatientID)
 	{
+		String returnObj = "";
 		PatientToProvider p2p = new PatientToProvider();
 
 		try
 		{
 			ArrayList<String> providers = p2p.FindAllProviders(szPatientID);
-			return ObjectToString(providers);
+			returnObj = ObjectToString(providers);
 		}
 		catch(Exception e)
 		{
-			return e.getMessage();
+			returnObj = e.getMessage();
 		}
+		Response response = Response.status(200).
+                entity(returnObj).
+                header("Access-Control-Allow-Origin", "*").build();
 	}
 
 	@GET
 	@Path("GetAllPatientsForProvider")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Object GetAllPatientsForProvider(@HeaderParam("ProviderID") String szProviderID)
+	public void GetAllPatientsForProvider(@HeaderParam("ProviderID") String szProviderID)
 	{
+		String returnObj = "";
 		PatientToProvider p2p = new PatientToProvider();
 
 		try
 		{
 			ArrayList<String> patients = p2p.FindAllPatients(szProviderID);
-			return ObjectToString(patients);
+			returnObj = ObjectToString(patients);
 		}
 		catch(Exception e)
 		{
-			return e.getMessage();
+			returnObj = e.getMessage();
 		}
+		Response response = Response.status(200).
+                entity(returnObj).
+                header("Access-Control-Allow-Origin", "*").build();
 	}
 
 	@POST
 	@Path("AddFeedbackForProvider")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Object AddFeedbackForProvider(@HeaderParam("PatientID") String szPatientID, @HeaderParam("ProviderID") String szProviderID, @HeaderParam("Feedback") String szfeedback)
+	public void AddFeedbackForProvider(@HeaderParam("PatientID") String szPatientID, @HeaderParam("ProviderID") String szProviderID, @HeaderParam("Feedback") String szfeedback)
 	{
+		String returnObj = "";
 		ProviderFeedback feedback = new ProviderFeedback();
 
 		try
 		{
 			if(!feedback.AddPatientToProviderFeedback(szPatientID, szProviderID, szfeedback))
-				return "{\"error\":\"Provider feedback not added to the system\"}";
+				returnObj = "{\"error\":\"Provider feedback not added to the system\"}";
 			else
-				return "{\"success\":\"Provider feedback added to the system\"}";	
+				returnObj = "{\"success\":\"Provider feedback added to the system\"}";	
 		}
 		catch(Exception e)
 		{
-			return e.getMessage();
+			returnObj = e.getMessage();
 		}
+		Response response = Response.status(200).
+                entity(returnObj).
+                header("Access-Control-Allow-Origin", "*").build();
 	}
 
 	@GET
 	@Path("ViewAllFeedbackForProvider")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Object ViewAllFeedbackForProvider(@HeaderParam("ProviderID") String szProviderID)
+	public void ViewAllFeedbackForProvider(@HeaderParam("ProviderID") String szProviderID)
 	{
+		String returnObj = "";
+		
 		ProviderFeedback feedback = new ProviderFeedback();
 
 		try
 		{
 			ArrayList<ProviderFeedback> patients = feedback.FindAllFeedbackForProvider(szProviderID);
-			return ObjectToString(patients);
+			returnObj = ObjectToString(patients);
 		}
 		catch(Exception e)
 		{
-			return e.getMessage();
+			returnObj = e.getMessage();
 		}
+		Response response = Response.status(200).
+                entity(returnObj).
+                header("Access-Control-Allow-Origin", "*").build();
 	}
 
 	@GET
 	@Path("ViewAllFeedbackGivenByPatient")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Object ViewAllFeedbackGivenByPatient(@HeaderParam("PatientID") String szPatientID)
+	public void ViewAllFeedbackGivenByPatient(@HeaderParam("PatientID") String szPatientID)
 	{
+		String returnObj = "";
+		
 		ProviderFeedback feedback = new ProviderFeedback();
 
 		try
 		{
 			ArrayList<ProviderFeedback> providers = feedback.FindAllFeedbackForPatient(szPatientID);
-			return ObjectToString(providers);
+			returnObj = ObjectToString(providers);
 		}
 		catch(Exception e)
 		{
-			return e.getMessage();
+			returnObj = e.getMessage();
 		}
+		Response response = Response.status(200).
+                entity(returnObj).
+                header("Access-Control-Allow-Origin", "*").build();
 	}
 
 	@GET
 	@Path("ViewAllFeedbackGivenByPatientForProvider")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Object ViewAllFeedbackGivenByPatientForProvider(@HeaderParam("PatientID") String szPatientID, @HeaderParam("ProviderID") String szProviderID)
+	public void ViewAllFeedbackGivenByPatientForProvider(@HeaderParam("PatientID") String szPatientID, @HeaderParam("ProviderID") String szProviderID)
 	{
+		String returnObj = "";
 		ProviderFeedback feedback = new ProviderFeedback();
 
 		try
 		{
 			ArrayList<ProviderFeedback> fdbk = feedback.GetAllFeedbackGivenByPatientForProvider(szPatientID, szProviderID);
-			return ObjectToString(fdbk);
+			returnObj = ObjectToString(fdbk);
 		}
 		catch(Exception e)
 		{
-			return e.getMessage();
+			returnObj = e.getMessage();
 		}
+		Response response = Response.status(200).
+                entity(returnObj).
+                header("Access-Control-Allow-Origin", "*").build();
 	}
 
 
 	@POST
 	@Path("AddAppointment")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Object AddAppointment(@HeaderParam("PatientID") String szPatientID, @HeaderParam("ProviderID") String szProviderID, @HeaderParam("DateTime") String szDateTime, @HeaderParam("Notes") String szNotes)
+	public void AddAppointment(@HeaderParam("PatientID") String szPatientID, @HeaderParam("ProviderID") String szProviderID, @HeaderParam("DateTime") String szDateTime, @HeaderParam("Notes") String szNotes)
 	{
+		String returnObj = "";
 		Appointment appointment = new Appointment();
 
 		try
 		{
 			if(!appointment.AddPatientToProviderAppointment(szPatientID, szProviderID, szNotes, szDateTime))
-				return "{\"error\":\"Provider appointment not added to the system\"}";
+				returnObj = "{\"error\":\"Provider appointment not added to the system\"}";
 			else
-				return "{\"success\":\"Provider appointment added to the system\"}";	
+				returnObj = "{\"success\":\"Provider appointment added to the system\"}";	
 		}
 		catch(Exception e)
 		{
-			return e.getMessage();
+			returnObj = e.getMessage();
 		}
+		Response response = Response.status(200).
+                entity(returnObj).
+                header("Access-Control-Allow-Origin", "*").build();
 	}
 
 	@GET
 	@Path("GetAppointment")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Object DeleteAppointment(@HeaderParam("PatientID") String szPatientID, @HeaderParam("ProviderID") String szProviderID)
+	public void DeleteAppointment(@HeaderParam("PatientID") String szPatientID, @HeaderParam("ProviderID") String szProviderID)
 	{
+		String returnObj = "";
 		Appointment appointment = new Appointment();
 
 		try
 		{
 			ArrayList<Appointment> apmt = appointment.GetAllAppointmentsGivenByPatientForProvider(szPatientID, szProviderID);
-			return ObjectToString(apmt);
+			returnObj = ObjectToString(apmt);
 		}
 		catch(Exception e)
 		{
-			return e.getMessage();
+			returnObj = e.getMessage();
 		}
+		Response response = Response.status(200).
+                entity(returnObj).
+                header("Access-Control-Allow-Origin", "*").build();
 	}
 
 	@POST
 	@Path("SendEmailToAnotherProvider")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Object SendEmailToAnotherProvider(@HeaderParam("PatientID") String szPatientID, @HeaderParam("OrigProviderID") String szOrigProviderID, @HeaderParam("NewProviderID") String szNewProviderID, @HeaderParam("EmailContent") String szEmailContent)
+	public void SendEmailToAnotherProvider(@HeaderParam("PatientID") String szPatientID, @HeaderParam("OrigProviderID") String szOrigProviderID, @HeaderParam("NewProviderID") String szNewProviderID, @HeaderParam("EmailContent") String szEmailContent)
 	{
+		String returnObj = "";
 		if(!NotificationService.Notify(szOrigProviderID, szNewProviderID, szPatientID, szEmailContent))
-			return "{\"error\":\"There was an error sending the notification\"}";
+			returnObj = "{\"error\":\"There was an error sending the notification\"}";
 		else
-			return "{\"success\":\"Notification has been sent successfully\"}";
+			returnObj = "{\"success\":\"Notification has been sent successfully\"}";
+		Response response = Response.status(200).
+                entity(returnObj).
+                header("Access-Control-Allow-Origin", "*").build();
 
 	}
 
 	@POST
 	@Path("AddMedicalRecord")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Object AddMedicalRecord(@HeaderParam("PatientID") String szPatientID, @HeaderParam("ProviderID") String szProviderID, @HeaderParam("MedicalRecordJSON") String szMedicalRecord)
+	public void AddMedicalRecord(@HeaderParam("PatientID") String szPatientID, @HeaderParam("ProviderID") String szProviderID, @HeaderParam("MedicalRecordJSON") String szMedicalRecord)
 	{
+		String returnObj = "";
 		//also store the timestamp
-		return "";
+		Response response = Response.status(200).
+                entity(returnObj).
+                header("Access-Control-Allow-Origin", "*").build();
 
 	}
 
 	@GET
 	@Path("ViewEntireMedicalHistory")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Object ViewEntireMedicalHistory(@HeaderParam("PatientID") String szPatientID)
+	public void ViewEntireMedicalHistory(@HeaderParam("PatientID") String szPatientID)
 	{
-		return "";
+		String returnObj = "";
+		Response response = Response.status(200).
+                entity(returnObj).
+                header("Access-Control-Allow-Origin", "*").build();
 
 	}
 
 	@GET
 	@Path("ViewLatestMedicalHistory")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Object ViewLatestMedicalHistory(@HeaderParam("PatientID") String szPatientID)
+	public void ViewLatestMedicalHistory(@HeaderParam("PatientID") String szPatientID)
 	{
-		return "";
+		String returnObj = "";
+		Response response = Response.status(200).
+                entity(returnObj).
+                header("Access-Control-Allow-Origin", "*").build();
 
 	}
 }
